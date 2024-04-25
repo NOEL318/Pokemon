5d95f4729f52ddd40cfa451da18ddc76463c438a
EmmaDuarte16 <emma.duartemo@udlap.mx>
2024-04-25 17:08:54 -0600

    Emmas work

--- src/main/java/poo/CreateFrame.java
+++ src/main/java/poo/CreateFrame.java
@@ -101,6 +101,7 @@ public class CreateFrame extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        new MisPokemones().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
